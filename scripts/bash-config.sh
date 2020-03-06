fde79db53050aeab382859f1c78910e9784f383d
Ondřej Sluka <ondras12345@gmail.com>
2020-03-06 17:26:45 +0100

    Update aliases

--- scripts/bash-config.sh
+++ scripts/bash-config.sh
@@ -2,4 +2,6 @@
 # test -f ~/scripts/bash-config.sh && . ~/scripts/bash-config.sh
 
 
+# https://sanctum.geek.nz/arabesque/vi-mode-in-bash/
+# Add `set editing-mode vi` to .inputrc for use in all tools using GNU Readline
 set -o vi
