9adf46f8dfdbab2869e65d8897b9f1af9005fe15
Ondřej Sluka <ondras12345@gmail.com>
2020-12-24 11:17:23 +0100

    Move some config files to .config

--- scripts/environment-vars.sh
+++ scripts/environment-vars.sh
@@ -4,4 +4,9 @@
 export VISUAL="vim"
 export EDITOR="$VISUAL"
 export LESS="-i -R -W"
+
+# XDG
+export XDG_CONFIG_HOME="$HOME/.config"
+export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+
 export TIME_STYLE=long-iso
