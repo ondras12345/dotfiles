4c30a161322ede8ae77dd810bf6f6ae35fd6881f
Ondřej Sluka <ondras12345@gmail.com>
2023-08-05 20:15:23 +0200

    Clean up $HOME by forcing XDG directories

--- scripts/environment-vars.sh
+++ scripts/environment-vars.sh
@@ -7,7 +7,10 @@ export LESS="-i -R -W"
 
 # XDG
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"  # needed for lesshst
 export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+export JUPYTER_PLATFORM_DIRS="1"
+export SQLITE_HISTORY=$XDG_DATA_HOME/sqlite_history
 
 # already set in .profile, but does not work when connecting via SSH:
 export PATH="$HOME/bin:$PATH"
