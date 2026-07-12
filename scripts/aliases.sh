a60b5913556f3f1d0bdd03807f6bf7d14093de3a
Ondřej Sluka <ondras12345@gmail.com>
2026-07-12 08:37:34 +0200

    Add vig and l shell aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -8,8 +8,10 @@ alias dotfilesvim='GIT_DIR=$HOME/.dotfiles-repo vim +G +only'
 alias dfg='dotfiles'
 alias dfv='dotfilesvim'
 alias ll='ls -lFah'
+alias l='ls -lFah'
 alias llh='ls -lFah --hyperlink=auto'
 alias la='ls -A'
+alias vig='vim +G +only'
 
 if command -v todo-txt &> /dev/null ; then
     alias t='todo-txt'
